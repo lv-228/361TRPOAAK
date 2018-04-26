ca2c1cf219e8bd7000fe3b77f30e8a6372204a2d
lv-228 <massefick@gmail.com>
2018-04-26 18:56:23 +0700

    ObjectInitializer

---
+++ ObjectInitializer/ObjectInitializer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point firstPoint = new ObjectInitializer.Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+
+            Point anotherPoint = new ObjectInitializer.Point(20, 20);
+            anotherPoint.DisplayStats();
+
+            Point finalPoint = new Point { X = 30, Y = 30 };
+            finalPoint.DisplayStats();
+            Console.ReadLine();
+
+            Rectangle myRect = new Rectangle
+            {
+                TopLeft = new Point { X = 10, Y = 10 },
+                BottomRight = new Point { X = 200, Y = 200 }
+            };
+        }
+    }
+}
---
+++ ObjectInitializer/ObjectInitializer/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializer
+{
+    class Rectangle
+    {
+        private Point topleft = new Point();
+        private Point bottomRight = new Point();
+
+        public Point TopLeft
+        {
+            get { return topleft; }
+            set { topleft = value; }
+        }
+
+        public Point BottomRight
+        {
+            get { return bottomRight; }
+            set { bottomRight = value; }
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("[TopLeft: {0}, {1}, {2} BottomRight: {3}, {4}, {5}",topleft.X,topleft.Y,bottomRight.X,bottomRight.Y);
+        }
+    }
+}

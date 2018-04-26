f30821d501f01a3ee410ff21d3ea5ddee3138bde
lv-228 <massefick@gmail.com>
2018-04-26 18:50:12 +0700

    Добавлена ConstData

---
+++ ConstData/ConstData/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    class MyMathClass
+    {
+        public const double PI = 3.14;
+        public readonly double g = 9.8;
+
+        public MyMathClass(double g)
+        {
+            this.g = g;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Значение PI: {0}",MyMathClass.PI);
+            Console.ReadLine();
+        }
+    }
+}

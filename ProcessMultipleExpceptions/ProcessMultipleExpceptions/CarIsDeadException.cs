06b9a536d517f7cc670f83cdb2f7fc67cf7a55f2
lv-228 <massefick@gmail.com>
2018-05-06 12:51:34 +0700

    5

---
+++ ProcessMultipleExpceptions/ProcessMultipleExpceptions/CarIsDeadException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMultipleExpceptions
+{
+    class CarIsDeadException : ApplicationException
+    {
+        private string messageDetails = String.Empty;
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+
+        public CarIsDeadException() { }
+        public CarIsDeadException(string message, string cause, DateTime time)
+            :base(message)
+        {
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+        public override string Message
+        {
+            get
+            {
+                return string.Format("Сообщение об ошибке в машине {0}", messageDetails);
+            }
+        }
+        
+    }
+}
---
+++ ProcessMultipleExpceptions/ProcessMultipleExpceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMultipleExpceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Rusty", 90);
+            try
+            {
+                myCar.Accelerate(-10);
+            }
+            catch(CarIsDeadException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                myCar.CrankTunes(false);
+            }
+            Console.ReadLine();
+        }
+    }
+}

327b6eae25d052fbadc8a9e2c5ceafc6f1b201a6
lv-228 <massefick@gmail.com>
2018-05-06 12:50:12 +0700

    Добавил 5 задание

---
+++ CustomException/CustomException/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    class Car
+    {
+        public const int MaxSpeed = 100;
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+        private bool carIsDead;
+        private Radio theMusicBox = new Radio();
+
+        public Car() { }
+        public Car(string name,int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+        public void CrankTunes(bool state)
+        {
+            theMusicBox.TurnOn(state);
+        }
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+                    //throw new Exception(string.Format("{0} привысила скорость!", PetName));
+                    CarIsDeadException ex = new CarIsDeadException(string.Format("{0} превысил скорость!", PetName),"You have a lead foot",DateTime.Now);
+                    ex.HelpLink = "http://odnoklasniki.ru";
+                    //ex.Data.Add("TimeStamp", string.Format("The car exploded at {0}", DateTime.Now));
+                    //ex.Data.Add("Cause", "You have a lead foot");
+                    throw ex;
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+
+    }
+}
---
+++ CustomException/CustomException/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Rusty", 90);
+            try
+            {
+                myCar.Accelerate(50);
+            }
+            catch(CarIsDeadException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ErrorTimeStamp);
+                Console.WriteLine(e.CauseOfError);
+            }
+            Console.ReadLine();
+        }
+    }
+}
